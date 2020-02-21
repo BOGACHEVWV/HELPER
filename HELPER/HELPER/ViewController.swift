a6607dcc7055ff433cc3df1b50b8a4e05996ab5a
Bogachev <bogachok-m@mail.ru>
2020-02-21 23:17:25 +0300

    1

--- HELPER/HELPER/ViewController.swift
+++ HELPER/HELPER/ViewController.swift
@@ -5,16 +5,20 @@
 //  Created by mac on 12/01/2020.
 //  Copyright © 2020 BOGACHEVWV. All rights reserved.
 //
+// MARK: - округление до сотых
+//private func string(from slider: UISlider) -> String {
+//    return String(format: "%.2f", slider.value)
+//}
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+// MARK: - Скрываем клавиатуру нажатием на "Done"
+//   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//       textField.resignFirstResponder()
+//       return true
+//   }
+//
+// MARK: - Скрытие клавиатуры по тапу за пределами Text Field
+//   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//       super.touchesBegan(touches, with: event)
+//
+//       view.endEditing(true) // Скрывает клавиатуру, вызванную для любого объекта
+//   }
