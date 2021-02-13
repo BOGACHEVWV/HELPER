8589ab83459d219ad4ccda0fcd6480cc5a2b5313
Bogachev <bogachok-m@mail.ru>
2021-02-13 13:15:11 +0300

    Update SwiftUI.swift

--- HELPER/HELPER/SwiftUI.swift
+++ HELPER/HELPER/SwiftUI.swift
@@ -6,36 +6,76 @@
 //  Copyright © 2021 BOGACHEVWV. All rights reserved.
 //
 
+/*
+ */
+
 // MARK: - Клавиатура скрывается тапом по экрану
 
-//Клавиатура скрывается тапом по экрану
-//extension UIApplication {
-//    func endEditing() {
-//        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-//    }
-//}
-// пишется к стеку, в котором долны произойти изменения
-//.onTapGesture {
-//    UIApplication.shared.endEditing()
-//    value = Double(TFtext) ?? 0
-//}
+/*
+ Клавиатура скрывается тапом по экрану
+ extension UIApplication {
+ func endEditing() {
+ sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+ }
+ }
+ пишется к стеку, в котором долны произойти изменения
+ .onTapGesture {
+ UIApplication.shared.endEditing()
+ value = Double(TFtext) ?? 0
+ }
+ */
 
-// MARK: - Модификаторы
-//расположить текст внутри TextField
-//.multilineTextAlignment(.trailing)
+
 
 // MARK: - Alert
-// вызвается модификатором к объекту в котором будут неправильно введеные значения
+/*
+ вызвается модификатором к объекту в котором будут неправильно введеные значения
+ 
+ .alert(isPresented: $showAlert, content: {
+ Alert(
+ title: Text("Wrong Format"),
+ message:Text ("Value from 0 to 255")
+ )
+ })
+ */
 
-//.alert(isPresented: $showAlert, content: {
-//    Alert(
-//        title: Text("Wrong Format"),
-//        message:Text ("Value from 0 to 255")
-//    )
-//})
-//
 // MARK: - onAppear
-// onAppear {}  - при появлении
+/*
+ onAppear {}  - при появлении
+ */
 
 // MARK: - округление до целых чисел
-// lround(Double)
+/*
+ lround(Double)
+ */
+
+// MARK: - создание собственноого модификатора
+/*
+ Изменение существующего модификатора
+ 
+ Image(systemName: "trash")
+     .resizable()
+     //                .frame(height: 400)
+     //                .modifier(FrameFromSize(size: size))
+     .frame(size)
+ 
+ 
+ struct FrameFromSize:ViewModifier {
+     var size: CGSize
+     func body(content: Content) -> some View {
+         content.frame(width: size.width, height: size.height)
+     }
+ }
+
+ extension View /* / Image */ {
+     func frame(_ size: CGSize) -> some View {
+         modifier(FrameFromSize(size: size))
+     }
+ }
+ 
+ */
+
+// MARK: - padding() Сразу во все встороны
+/*
+ .padding(EdgeInsets(top: <#T##CGFloat#>, leading: <#T##CGFloat#>, bottom: <#T##CGFloat#>, trailing: <#T##CGFloat#>))
+ */
