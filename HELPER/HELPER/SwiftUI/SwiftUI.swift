0af8047155efa8b6720d373ec85da2245fcc2228
Bogachev <bogachok-m@mail.ru>
2021-03-04 23:33:12 +0300

    NavigationView
    Change Navi background and title color

--- HELPER/HELPER/SwiftUI/SwiftUI.swift
+++ HELPER/HELPER/SwiftUI/SwiftUI.swift
@@ -238,3 +238,55 @@
  }
  
  */
+
+// MARK: - NavigationView изменить цвет navigationView (в том числе при .inline)
+/*
+ https://stackoverflow.com/questions/56505528/swiftui-update-navigation-bar-title-color
+ 
+ struct NavigationBarModifier: ViewModifier {
+
+     var backgroundColor: UIColor?
+     var titleColor: UIColor?
+
+     init(backgroundColor: UIColor?, titleColor: UIColor?) {
+         self.backgroundColor = backgroundColor
+         let coloredAppearance = UINavigationBarAppearance()
+         coloredAppearance.configureWithTransparentBackground()
+         coloredAppearance.backgroundColor = backgroundColor
+         coloredAppearance.titleTextAttributes = [.foregroundColor: titleColor ?? .white]
+         coloredAppearance.largeTitleTextAttributes = [.foregroundColor: titleColor ?? .white]
+
+         UINavigationBar.appearance().standardAppearance = coloredAppearance
+         UINavigationBar.appearance().compactAppearance = coloredAppearance
+         UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+     }
+
+     func body(content: Content) -> some View {
+         ZStack{
+             content
+             VStack {
+                 GeometryReader { geometry in
+                     Color(self.backgroundColor ?? .clear)
+                         .frame(height: geometry.safeAreaInsets.top)
+                         .edgesIgnoringSafeArea(.top)
+                     Spacer()
+                 }
+             }
+         }
+     }
+ }
+
+ extension View {
+
+     func navigationBarColor(backgroundColor: UIColor?, titleColor: UIColor?) -> some View {
+         self.modifier(NavigationBarModifier(backgroundColor: backgroundColor, titleColor: titleColor))
+     }
+
+ }
+ 
+ After that, apply like this:
+
+ .navigationBarColor(backgroundColor: .clear, titleColor: .white)
+ 
+ */
+
